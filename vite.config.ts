7b1ff4aa5d83a2055dbc2cf7a0c6778f94ad6d01
Trung Huỳnh <19520317@ms.uit.edu.vn>
2022-04-05 23:53:58 +0700

    Add svgr

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import vitePluginImp from "vite-plugin-imp";
+import svgr from "vite-plugin-svgr";
 
 function pathResolve(dir: string) {
   return resolve(__dirname, ".", dir);
@@ -29,6 +30,7 @@ export default defineConfig({
         },
       ],
     }),
+    svgr(),
   ],
   css: {
     preprocessorOptions: {
