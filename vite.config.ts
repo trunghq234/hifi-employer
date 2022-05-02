ed5b2f2b6beeba4c9518261e86bf86cd255ef8b4
Trung Huỳnh <19520317@ms.uit.edu.vn>
2022-05-02 15:35:07 +0700

    config: change variables

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,10 @@ export default defineConfig({
         javascriptEnabled: true,
         modifyVars: {
           "primary-color": "#683bc9",
+          "border-radius-base": "4px",
+          "card-shadow":
+            "box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+          "tabs-card-head-background": "#F3F3F3",
         },
       },
     },
