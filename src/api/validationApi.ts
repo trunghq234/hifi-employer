1a196ae086ce7577f47b248be34b870c3d65f491
Trung Huỳnh <19520317@ms.uit.edu.vn>
2022-06-12 15:54:54 +0700

    fix base url

--- src/api/validationApi.ts
+++ src/api/validationApi.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
-const baseURL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
 const validationApi = {
   checkEmail: async (email: string) => {
     const {
